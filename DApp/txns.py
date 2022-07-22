d21af81f073d1219e13f5dd66711f3c1d78c6870
PriyathamVarma <priyatham002@gmail.com>
2022-07-22 16:34:45 +0100

    new file added for txns retrieving

--- DApp/txns.py
+++ DApp/txns.py
@@ -1,4 +1,4 @@
-# This script is for getting account details from the blockchain
+# This script is for getting txn details and various key:value pairs with the transaction
 
 # Imports
 from web3 import Web3
@@ -14,21 +14,22 @@ alchemy_api_key = os.getenv('ALCHEMY_API_KEY')
 # metamask address
 wallet_address  = os.getenv('WALLET_ADDRESS')
 
-# print(alchemy_api_key)
-# print(wallet_address)
-
 # Connect to the blockchain
 web3Object = Web3(Web3.HTTPProvider(alchemy_api_key))
-
-
-# Get the balance of the wallet
-balance = web3Object.eth.getBalance(wallet_address)
-# print(balance)
 
 # Get the transaction details
 # see to it that txn is in polygon testnet
 txnDetails = web3Object.eth.get_transaction('0x2580e1b6453fe2cd6e941197aa6d9ac93de182e0d7df8ad00bcd0456a74a04d5')
-print(txnDetails)
+print(type(txnDetails)) # Class type <class 'dict'>
+
+# Accessing dictionary keys
+print(txnDetails['blockHash'])
+
+# Accessing dictionary values
+print(txnDetails['blockNumber'])
+
+# Accessing data from the dictionary
+print(txnDetails['from'])
 
 
 
